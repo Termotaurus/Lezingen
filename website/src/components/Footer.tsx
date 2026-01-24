b0df96d6723273ae9314e2fe1dc8e9d7f0de20b0
TOM TERMOTE <tom.termote@vives.be>
2026-01-24 19:20:59 +0100

    href

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="bg-gray-100 py-12 mt-20 border-t border-gray-200">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-6">
         <div className="flex flex-col items-center md:items-start">
-           <Link href="www.vives.be">
+           <a href="www.vives.be">
            <Image
             src="/images/CONTINUE_VIVES_RGB_LOGO.png"
             alt="VIVES Logo"
@@ -14,7 +13,7 @@ export default function Footer() {
             height={50}
             className="h-10 w-auto object-contain mb-4"
           />
-          </Link>
+          </a>
           <p className="text-gray-600 text-sm">
             © {new Date().getFullYear()} Tom Termote. All rights reserved.
           </p>
